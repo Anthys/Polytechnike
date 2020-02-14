6bf5b11bfb73ebc5928c198817a2a9efff8d5139
anthys <julbarres@gmail.com>
2020-02-14 16:23:59 +0100

    yesy

--- test.py
+++ test.py
@@ -11,14 +11,29 @@ def load(adr):
     for x in range(nb_photos):
         d = dict()
         indice = x
-        type = txt[x + 1][0]
+        typez = txt[x + 1][0]
         nb_arg = int(txt[x + 1][1])
         arg = set()
         for y in range(2, nb_arg + 2):
             arg.add(txt[x + 1][y])
         d["indice"] = indice
-        d["type"] = type
+        d["type"] = typez
         d["nb_arg"] = nb_arg
         d["arg"] = arg
         photos.append(d)
     return photos
+
+
+def dif_args(list_photo):
+  args = []
+  for i in list_photo:
+    for j in i["arg"]:
+      if not j in args:
+        args += [j]
+  
+  return len(args)
+
+
+
+a = load("qualification_round_2019.in/e_shiny_selfies.txt")
+print(dif_args(a))
