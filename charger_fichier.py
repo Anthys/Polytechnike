ee3aadf2cd92dcecfe5edac94a71a6ac6ed3a31b
Maxime Moutet <55545412+MaximeMoutet13@users.noreply.github.com>
2020-02-14 16:46:23 +0100

    Update and rename test.py to charger_fichier.py

--- charger_fichier.py
+++ charger_fichier.py
@@ -1,6 +1,8 @@
-__author__ = "maxime"
-__file__ = "test.py"
-__date__ = "14/02/20"
+a = "a_example.txt"
+b = "b_lovely_landscapes.txt"
+c = "c_memorable_moments.txt"
+d = "d_pet_pictures.txt"
+e = "e_shiny_selfies.txt"
 
 
 def load(adr):
@@ -11,29 +13,25 @@ def load(adr):
     for x in range(nb_photos):
         d = dict()
         indice = x
-        typez = txt[x + 1][0]
+        type = txt[x + 1][0]
         nb_arg = int(txt[x + 1][1])
         arg = set()
         for y in range(2, nb_arg + 2):
             arg.add(txt[x + 1][y])
         d["indice"] = indice
-        d["type"] = typez
+        d["typ"] = type
         d["nb_arg"] = nb_arg
         d["arg"] = arg
         photos.append(d)
     return photos
 
 
-def dif_args(list_photo):
-  args = []
-  for i in list_photo:
-    for j in i["arg"]:
-      if not j in args:
-        args += [j]
-  
-  return len(args)
+def liste_verticale(photos):
+    res = []
+    for d in photos:
+        if d["typ"] == "V":
+            res.append(d)
+    return res
 
-
-
-a = load("qualification_round_2019.in/e_shiny_selfies.txt")
-print(dif_args(a))
+p = load(a)
+print(liste_verticale(p))
