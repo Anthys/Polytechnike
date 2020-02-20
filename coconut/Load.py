182daea615fe6db26dd36f59b41ec37f0ffe24fb
maxime <maxime.moutet@etu.univ-amu.fr>
2020-02-20 17:59:58 +0100

    y

---
+++ coconut/Load.py
@@ -0,0 +1,12 @@
+__author__ = "maxime"
+__file__ = "Load.py"
+__date__ = "20/02/2020"
+
+
+def load(fichier):
+    txt = [line.split() for line in fichier]
+    for i, x in enumerate(txt[1]):
+        txt[1][i] = int(x)
+    pizzas = txt[1]
+    nbparts = int(txt[0][0])
+    return pizzas, nbparts
