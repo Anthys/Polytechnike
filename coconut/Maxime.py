895a4af2924aedd59078f963960296aa33764e50
maxime <maxime.moutet@etu.univ-amu.fr>
2020-02-20 21:06:45 +0100

    dz

--- coconut/Maxime.py
+++ coconut/Maxime.py
@@ -15,3 +15,19 @@ def tri_books(liste, books_score):
                 lib["books_ind"][i], lib["books_ind"][maxim] = lib["books_ind"][maxim], lib["books_ind"][i]
     return liste
 
+
+def compute_sum_score(liste, scores):
+    for libr in liste:
+        sum = 0
+        for book in libr["books_ind"]:
+            sum += scores[book]
+        libr["sum_score"] = sum
+    return liste
+
+
+def reorder_libraries(liste):
+    for i in range(len(liste) - 1):
+        if len(liste[i + 1]["books_ind"]) > len(liste[i]["books_ind"]):
+            liste[i + 1], liste[i] = liste[i], liste[i + 1]
+    return liste
+
