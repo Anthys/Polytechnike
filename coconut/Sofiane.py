2b2bd552935e39c8cab8d1a924283c1d725f0dd9
sofianeaissani <sofiane.aissani@hotmail.com>
2020-02-20 21:54:39 +0100

    remove from k

--- coconut/Sofiane.py
+++ coconut/Sofiane.py
@@ -20,21 +20,44 @@ def remove_multiple_occurrencies(listoflibraries, books_score):
         # print(" ",str(Dict))
         j += 1
     return listoflibraries
-
+'''
 L = [[1, 2, 4], [0, 4, 2]] #, [0, 0, 0, 0, 0], [1, 3, 4]]
 H = []
 for i in L:
     H += [{"books_ind":i}]
 print(remove_multiple_occurrencies(H, [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]))
+'''
 
+def remove_occurrencies_from_K(listoflibraries, books_score, K):
+    Dict = dict()
+    for i in listoflibraries[K]["books_ind"]:
+        Dict[i] = False
+    for library in listoflibraries[K:]:
+        i = 0
+        while i < len(library["books_ind"]):
+            book_ind = library["books_ind"][i]
+            print(" ", str(library))
+            if book_ind in list(Dict.keys()) and Dict[book_ind]:
+                del library["books_ind"][i]
+            else:
+                i += 1
+            Dict[book_ind] = True
+    return listoflibraries
 
-"""
-#a = "a_example.txt"
+L = [[1, 2, 4], [0, 4, 2], [0, 0, 0, 0, 0], [1, 3, 4]]
+H = []
+for i in L:
+    H += [{"books_ind":i}]
+print(remove_occurrencies_from_K(H, [1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 1))
+
+'''
+a = "a_example.txt"
 
 file1 = open(a)
 values, file1 = Load.load(file1)
 b = Jules.tri_days_sign_up(file1)
 c = Maxime.tri_books(b, values)
 
-Julien.print_output_file(b, values)"""
+Julien.print_output_file(b, values)
 
+'''
