8f4cdb8c59e4fae65028cc28037bc1e2bc9c85cc
julesmichaud <jules.michaud@yahoo.fr>
2020-02-20 20:02:12 +0100

    aaa

--- coconut/Jules.py
+++ coconut/Jules.py
@@ -2,10 +2,17 @@ __author__ = 'julesmichaud'
 __filename__ = 'Jules.py'
 __date__ = '20/02/20'
 
-from coconut import Load
-
 
 def tri_days_sign_up(list):
     list.sort(key=lambda x: x["days_signup"], reverse=False)
     return list
 
+
+def tri_1(list):
+    list.sort(key=lambda x: (x["sum_score"]/(x["days_signup"]+x["books_per_day"])), reverse=True)
+    return list
+
+
+def tri_2(list):
+    list.sort(key=lambda x: (x["sum_score"]/(x["days_signup"]) - x["books_per_day"]), reverse=True)
+    return list
