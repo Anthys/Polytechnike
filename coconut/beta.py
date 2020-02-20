92b04aa0585d71feecfa952329fdd85abc5d907c
julesmichaud <jules.michaud@yahoo.fr>
2020-02-20 20:18:09 +0100

    aaa

---
+++ coconut/beta.py
@@ -0,0 +1,12 @@
+__author__ = 'julesmichaud'
+__filename__ = 'beta.py.py'
+__date__ = '20/02/20'
+
+import Jules, Julien, Load
+
+a = "a_example.txt"
+
+file1 = open(a)
+values, file1 = Load.load(file1)
+b = Jules.tri_2(file1)
+Julien.print_output_file(b, values)
