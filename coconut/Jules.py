7f8a111f8518d7d1d1f16b0c0b468c58f1f5a3b0
julesmichaud <jules.michaud@yahoo.fr>
2020-02-20 19:22:06 +0100

    aaa

--- coconut/Jules.py
+++ coconut/Jules.py
@@ -2,3 +2,9 @@ __author__ = 'julesmichaud'
 __filename__ = 'Jules.py'
 __date__ = '20/02/20'
 
+from coconut import Load
+
+
+def tri_days_sign_up(list):
+    list.sort(key=lambda x: x["days_signup"], reverse=True)
+    return list
